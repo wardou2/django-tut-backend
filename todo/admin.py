add6346a4e09e0ac88bf73b4eeabec55187c0838
wardou2 <ward92@gmail.com>
2019-08-22 21:52:13 -0700

    admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Todo
 
-class TodoAdmin(admin.ModelAdmin):  # add this
-  list_display = ('title', 'description', 'completed') # add this
+class TodoAdmin(admin.ModelAdmin):
+  list_display = ('title', 'description', 'completed')
 
-# Register your models here.
-admin.site.register(Todo, TodoAdmin) # add this
+admin.site.register(Todo, TodoAdmin)
